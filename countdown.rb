b7ae100f1e08d4c6ded60e282a0bcac520d27cb0
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 12:20:53 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number = number - 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
