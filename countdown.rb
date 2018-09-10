c9e13045900282a033523cc7731163ab2c7295db
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 12:11:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   number = gets.chomp
   while number > 0
-    puts "#{number}" + " SECOND(S)!"
+    puts "#{number}  SECOND(S)!"
     int -= 1
   end
   puts "HAPPY NEW YEAR!"
